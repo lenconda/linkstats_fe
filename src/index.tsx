5e27e4d0574d80df70c2a9ea146f4fac9c2eb3e3
lenconda <1053464288@qq.com>
2019-06-28 22:30:44 +0800

    For eject code

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import zhCN from 'antd/lib/locale-provider/zh_CN'
+import { LocaleProvider } from 'antd'
+
+ReactDOM.render(
+    <LocaleProvider locale={zhCN}>
+      <App />
+    </LocaleProvider>,
+    document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Login: React.FC = (props): JSX.Element => {
+  console.log(props)
+  return (
+      <div></div>
+  )
+}
