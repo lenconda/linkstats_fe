3fdc7ae1d3584e7c9d6c8180ece0cc3d2998376d
lenconda <1053464288@qq.com>
2019-06-29 17:05:47 +0800

    fix: remove theme.css

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import './theme.css'
 
 import zhCN from 'antd/lib/locale-provider/zh_CN'
 import { LocaleProvider } from 'antd'
