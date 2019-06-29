782c9520929c3308a799b533d1843234a2104967
lenconda <1053464288@qq.com>
2019-06-30 00:28:25 +0800

    feat: finish login page ui design

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,30 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Button,
   Input,
   Row,
   Col,
-  Divider,
-  Card
+  Card,
+  Icon,
+  Form,
+  Checkbox
 } from 'antd'
 import queryString from 'query-string'
 import './Login.css'
 
-const Login: React.FC = (props: any): JSX.Element => {
+const LoginForm: React.FC = (props: any): JSX.Element => {
+  const { getFieldDecorator } = props.form
+
+  const [email, setEmail] = useState('')
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    props.form.validateFields((err: any, values: any) => {
+      if (!err) {
+        console.log('Received values of form: ', values)
+      }
+    })
+  }
+
   return (
-      <Row style={{height: '100%'}}>
-        <Col xs={24} sm={24} md={12} lg={14} xl={10} className={'banner'}>
-          <h1>LinkStats</h1>
-        </Col>
-        <Col xs={24} sm={24} md={8} lg={6} xl={4}>
-          <Card>
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
+      <Row className={'login-box-wrapper'}>
+        <Col xs={24} sm={16} md={14} lg={10} xl={6}>
+          <Card title={'验证你的凭据'}
+                type={'inner'}
+                style={{textAlign: 'left'}}
+                actions={[<a onClick={() => console.log(email)}>忘记密码</a>, <a>注册一个账户</a>]}
+          >
+            <Form onSubmit={handleSubmit} className={'login-form'}>
+              <Form.Item>
+                {getFieldDecorator('username', {
+                  rules: [{ required: true, message: '请输入你的邮箱地址' }],
+                })(
+                    <Input
+                        prefix={<Icon type={'user'} style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        placeholder={'邮箱地址'}
+                        onChange={e => setEmail(e.target.value)}
+                    />,
+                )}
+              </Form.Item>
+              <Form.Item>
+                {getFieldDecorator('password', {
+                  rules: [{ required: true, message: '请输入你的密码' }],
+                })(
+                    <Input
+                        prefix={<Icon type={'lock'} style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        type={'password'}
+                        placeholder={'密码'}
+                    />,
+                )}
+              </Form.Item>
+              <Form.Item>
+                {getFieldDecorator('remember', {
+                  valuePropName: 'checked',
+                  initialValue: true,
+                })(<Checkbox>使我保持登录状态</Checkbox>)}
+                <section>
+                  <Button type={'primary'} block={true} htmlType={'submit'} className={'login-form-button'}>
+                    登录
+                  </Button>
+                </section>
+              </Form.Item>
+            </Form>
           </Card>
         </Col>
       </Row>
   )
 }
+
+const Login = Form.create({name: 'login'})(LoginForm)
 
 export default Login
