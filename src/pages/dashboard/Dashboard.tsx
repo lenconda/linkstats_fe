c66a9e1bb339fdde24a8b76cd3ee0b096a9516e2
lenconda <1053464288@qq.com>
2019-06-30 23:53:11 +0800

    fix: login redirect param dismatch

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import http from '../../util/http'
 
 const Dashboard: React.FC = (): JSX.Element => {
-  http.get('/api/record/export')
-      .then(res => console.log(res.data))
+  http.get('/api/profile/info')
 
   return <div></div>
 }
