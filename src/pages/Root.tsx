3b316904072b760d09800ad915535e907b96990e
lenconda <1053464288@qq.com>
2019-07-04 15:48:05 +0800

    feat: main page

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -2,15 +2,24 @@ import React from 'react'
 import {
   Divider,
   Icon,
-  Button
+  Button,
+  Typography
 } from 'antd'
 import './Root.css'
 import { history } from '../App'
 
+const {
+  Title
+} = Typography
+
 const Root: React.FC = (): JSX.Element =>
     <div className={'root-container'}>
       <div>
-        <h1>LinkStats</h1>
+        <Title level={1} className={'title'}>
+          <img src={'/logo.svg'} alt={'logo'} height={32}/>
+          <Divider type={'vertical'}/>
+          LinkStats
+        </Title>
         <p>轻量的链接访问数据记录工具</p>
         <span>
           <a href="https://docs.linkstats.cc" target={'_blank'}><Icon type="file-text" />&nbsp;文档</a>
