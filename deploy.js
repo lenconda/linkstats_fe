da3eab79a107fd523df219296bf91070ea28ae65
lenconda <1053464288@qq.com>
2019-07-04 17:13:47 +0800

    feat: add ci configs

---
+++ deploy.js
@@ -0,0 +1,20 @@
+const deploy = require('ali-oss-deploy')
+
+deploy({
+  path: '../build',
+  ossConfig: {
+    region: 'oss-ap-northeast-1',
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    accessKeySecret: process.env.ACCESS_KEY_SECRET,
+  },
+  bucket: {
+    pro: {
+      name: 'linkstats-static',
+      refreshPath: '',
+    },
+    test: {
+      name: 'linkstats-static',
+      projectPath: ''
+    },
+  },
+})
