4e95c8ba5ba33d2710aeb655c2612160828aecf4
lenconda <1053464288@qq.com>
2019-07-11 10:22:31 +0800

    fix: track code

--- public/js/code.js
+++ public/js/code.js
@@ -1,12 +1,12 @@
 (function() {
   window.send = function(id, href) {
     var xhr;
-    xhr.setRequestHeader("Content-Type", "application/json");
     if (window.XMLHttpRequest)
       xhr = new XMLHttpRequest();
     else
       // eslint-disable-next-line no-undef
       xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    xhr.setRequestHeader("Content-Type", "application/json");
     xhr.open("POST", "/api/redirect/code?id=" + id, true);
     xhr.send({ href: href });
   }
